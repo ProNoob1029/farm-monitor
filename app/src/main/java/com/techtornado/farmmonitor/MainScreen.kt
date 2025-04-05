5892b60c73d6c35268043fec42b774b9a20f8e84
ProNoob1029 <pronoob1029@gmail.com>
2025-04-05 18:54:46 +0300

    drawing land

--- app/src/main/java/com/techtornado/farmmonitor/MainScreen.kt
+++ app/src/main/java/com/techtornado/farmmonitor/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.techtornado.farmmonitor
 
+import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -10,6 +12,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -55,10 +61,25 @@ fun MainScreen(
             .verticalScroll(rememberScrollState())
         ,
     ) {
-        Button(
-            onClick = onGet
-        ) {
-            Text("GET")
+        var editMode by remember {
+            mutableStateOf(false)
+        }
+        Row {
+            Button(
+                onClick = onGet
+            ) {
+                Text("GET")
+            }
+            Button(
+                onClick = {
+                    editMode = !editMode
+                }
+            ) {
+                Text("EDIT")
+            }
+        }
+        val editablePolygon = remember {
+            mutableStateListOf<LatLng>()
         }
         MapsScreen(
             modifier = Modifier
@@ -68,7 +89,15 @@ fun MainScreen(
                 polygon.geo_json.geometry.coordinates[0].map { points ->
                     LatLng(points[1], points[0])
                 }
-            }
+            },
+            onMapClick = { latLng ->
+                Log.d("MainScreen", latLng.toString())
+                if (editMode) {
+                    editablePolygon.add(latLng)
+                }
+                Log.d("MainScreen", editablePolygon.toString())
+            },
+            polygon = editablePolygon.toList()
         )
         Text(weather.toString())
     }
--- app/src/main/java/com/techtornado/farmmonitor/MapsScreen.kt
+++ app/src/main/java/com/techtornado/farmmonitor/MapsScreen.kt
@@ -3,6 +3,7 @@ package com.techtornado.farmmonitor
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
@@ -14,7 +15,9 @@ import com.google.maps.android.compose.rememberMarkerState
 @Composable
 fun MapsScreen(
     modifier: Modifier = Modifier,
-    polymaps: List<List<LatLng>> = emptyList()
+    polymaps: List<List<LatLng>> = emptyList(),
+    onMapClick: (LatLng) -> Unit,
+    polygon: List<LatLng>
 ) {
     val cluj = LatLng(46.77, 23.64)
     val clujMarkerState = rememberMarkerState(position = cluj)
@@ -23,7 +26,8 @@ fun MapsScreen(
     }
     GoogleMap(
         modifier = modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
+        cameraPositionState = cameraPositionState,
+        onMapClick = onMapClick
     ) {
 
         Marker(
@@ -37,5 +41,11 @@ fun MapsScreen(
                 points = points
             )
         }
+
+        Polygon(
+            points = polygon,
+            geodesic = true,
+            fillColor = Color.Black.copy(alpha = 0.2f)
+        )
     }
 }
