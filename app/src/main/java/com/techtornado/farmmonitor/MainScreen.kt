f2ea012d5c40cc9053357f3c39bd1b6d56c76493
ProNoob1029 <pronoob1029@gmail.com>
2025-04-05 16:37:08 +0300

    we have teren

--- app/src/main/java/com/techtornado/farmmonitor/MainScreen.kt
+++ app/src/main/java/com/techtornado/farmmonitor/MainScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -11,8 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.google.android.gms.maps.model.LatLng
 import com.techtornado.farmmonitor.data.Polygon
 
 @Composable
@@ -35,10 +39,7 @@ fun MainScreen(
     onGet: () -> Unit,
     data: List<Polygon>
 ) {
-    MapsScreen(
-        modifier = Modifier.fillMaxSize()
-    )
-    /*Column(
+    Column(
         modifier = modifier
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
@@ -49,7 +50,19 @@ fun MainScreen(
         ) {
             Text("GET")
         }
+        MapsScreen(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(500.dp),
+            /*polymaps = data.firstOrNull()?.geo_json?.geometry?.coordinates?.firstOrNull()?.map {
+                LatLng(it[1], it[0])
+            } ?: emptyList()*/
+            polymaps = data.map { polygon ->
+                polygon.geo_json.geometry.coordinates[0].map { points ->
+                    LatLng(points[1], points[0])
+                }
+            }
+        )
         Text(data.toString())
-
-    }*/
+    }
 }
--- app/src/main/java/com/techtornado/farmmonitor/MapsScreen.kt
+++ app/src/main/java/com/techtornado/farmmonitor/MapsScreen.kt
@@ -7,24 +7,35 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 
 @Composable
-fun MapsScreen(modifier: Modifier = Modifier) {
-    val singapore = LatLng(1.35, 103.87)
-    val singaporeMarkerState = rememberMarkerState(position = singapore)
+fun MapsScreen(
+    modifier: Modifier = Modifier,
+    polymaps: List<List<LatLng>> = emptyList()
+) {
+    val cluj = LatLng(46.77, 23.64)
+    val clujMarkerState = rememberMarkerState(position = cluj)
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition.fromLatLngZoom(cluj, 10f)
     }
     GoogleMap(
         modifier = modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
     ) {
+
         Marker(
-            state = singaporeMarkerState,
-            title = "Singapore",
-            snippet = "Marker in Singapore"
+            state = clujMarkerState,
+            title = "Cluj",
+            snippet = "Marker in Cluj"
         )
+
+        for (points in polymaps) {
+            Polygon(
+                points = points
+            )
+        }
     }
 }
