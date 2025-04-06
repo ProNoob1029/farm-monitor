e4138088b76908d7ece946e707b69f35fbb60ab8
lamadalaba <rracovictor@gmail.com>
2025-04-06 11:37:39 +0300

    sogos

--- app/src/main/java/com/techtornado/farmmonitor/screens/FirstScreen.kt
+++ app/src/main/java/com/techtornado/farmmonitor/screens/FirstScreen.kt
@@ -1,18 +1,24 @@
 package com.techtornado.farmmonitor.screens
 
 import android.util.Log
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,9 +27,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCompositionContext
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -36,6 +47,7 @@ import com.techtornado.farmmonitor.data.Weather
 import com.techtornado.farmmonitor.data.Polygon
 import com.techtornado.farmmonitor.data.Soil
 import com.techtornado.farmmonitor.data.Stat
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 
 @Composable
@@ -43,17 +55,37 @@ fun FirstScreen(
     modifier: Modifier = Modifier,
     viewModel: FirstViewModel = viewModel()
 ){
-    Column {
+    Column(modifier.padding(16.dp)) {
 
         Text("IONELA" ,
-            fontFamily = FontFamily(Font(R.font.cookie_run_regular)),
-            style = MaterialTheme.typography.headlineLarge
+
+            style = MaterialTheme.typography.headlineLarge ,
+            fontStyle = FontStyle.Italic
         )
 
         Spacer(Modifier.height(16.dp))
 
-        OutlinedButton(onClick = {}) {
-            Text("Add your first field")
+        Box(
+            modifier=Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        )
+        {
+            Surface(
+                onClick = {},
+                color = MaterialTheme.colorScheme.primaryContainer,
+                shape = RoundedCornerShape(16.dp),
+                modifier = Modifier.size(200.dp,200.dp),
+
+                ) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    Text(
+                        "Add your first field",
+                    )
+                }
+            }
         }
 
     }
